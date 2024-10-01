de322f99298c30bf1dd4892a506174b629ede6de
GIOVANiJFM <giovani.mendes2008@gmail.com>
2024-10-01 17:23:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  alert(`Obrigado, ${name}! Sua mensagem foi enviada com sucesso.`);
+
+  document.getElementById('contactForm').reset();
+});
+
+alert("Bem vindo ao museu");
+
+var resposta = confirm("Você deseja sair?");
+if (resposta) {
+alert("Você clicou em OK!");
+} else {
+alert("Você clicou em Cancelar!");
+}
